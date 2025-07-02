ab151931761cc71a931ec6feb0347d73628772c0
Guillaume Beuriot <gbeuriot.95@gmail.com>
2025-07-02 07:37:16 +0200

    page.tsx
    - Initial commit working on a simple task management tool

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
+'use client'
+import { useRef, useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+  const [tasks, setTasks] = useState<string[]>([])
+  const addTask = () => {
+    if (taskInput.current?.value) {
+      setTasks([...tasks, taskInput.current?.value])
+      taskInput.current!.value = ''
+    }
+  }
+
+  const deleteTask = (index: number) => {
+    setTasks(tasks.filter((_, i) => i !== index))
+  }
+
+  const taskInput = useRef<HTMLInputElement>(null)
+
+  const tasksTable = () => {
+    return (
+      <div className='flex flex-col gap-2 w-[300px]'>
+        {tasks.map((task, index) => (
+          <div className='border-2 border-gray-300 rounded-md p-2' key={index}>
+            <div className='flex items-center justify-between'>
+              <div>{task}</div>
+              <button className='bg-red-500 text-white  rounded-md w-[20px] h-[20px] hover:bg-red-600 cursor-pointer' onClick={() => deleteTask(index)}>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  const keyDownAddTask = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      addTask()
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold mt-10">Daily Flow</h1>
+
+      <div className='flex flex-col items-center justify-center mt-10'>
+        <h2 className="text-center text-2xl font-bold my-5">What are we doing today?</h2>
+        <div className="flex items-center justify-center gap-2">
+          <input type="text" placeholder="Task" ref={taskInput} onKeyDown={keyDownAddTask} className=" p-2 rounded-md bg-white w-[500px] h-[50px] text-black" />
+          <button className="bg-blue-500 text-white text-center rounded-md w-[50px] h-[50px] text-4xl" onClick={addTask} >+</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='flex flex-col items-center justify-center mt-10'>
+          <h2 className="text-center text-2xl font-bold my-5">Tasks</h2>
+          {tasksTable()}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
